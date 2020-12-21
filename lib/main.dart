1c11d46d48e848abe2dd816c6891d073b8fe2735
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-21 22:07:38 +0800

    added row and column

--- lib/main.dart
+++ lib/main.dart
@@ -9,28 +9,47 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Fundamental"),
-        ),
-        body: Center(
-          child: Container(
-              color: Colors.lightBlue,
-              width: 150,
-              height: 50,
-              child: Text(
-                "Hello World, Are you ready for upcoming challenges",
-                maxLines: 3,
-                overflow: TextOverflow.visible,
-                softWrap: true,
-                textAlign: TextAlign.right,
-                style:
-                    TextStyle(color: Colors.white, 
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w700,
-                    fontSize: 16),
-              )),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("Flutter Fundamental"),
+          ),
+          body: Column(
+            //y-axis
+            mainAxisAlignment: MainAxisAlignment.center,
+            //x-axis
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("data"),
+              Text("data"),
+              Text("data"),
+              Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text("row-data"),
+                  Text("row-data"),
+                  Text("row-data"),
+                ],
+              )
+            ],
+          )
+          // Center(
+          //   child: Container(
+          //       color: Colors.lightBlue,
+          //       width: 150,
+          //       height: 50,
+          //       child: Text(
+          //         "Hello World, Are you ready for upcoming challenges",
+          //         maxLines: 3,
+          //         overflow: TextOverflow.visible,
+          //         softWrap: true,
+          //         textAlign: TextAlign.right,
+          //         style: TextStyle(
+          //             color: Colors.white,
+          //             fontStyle: FontStyle.italic,
+          //             fontWeight: FontWeight.w700,
+          //             fontSize: 16),
+          //       )),
+          // ),
+          ),
     );
   }
 }
