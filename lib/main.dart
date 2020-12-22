f3ec0f8c50a208fdcc91a711d87dcbc9f8bd6fbd
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-22 13:51:24 +0800

    added animation and gesture detector

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,70 +12,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Widget> widgets = [];
-  int counter = 1;
-  void onButton1Clicked() {
-    setState(() {
-      widgets.add(Text("Data " + counter.toString()));
-      counter += 1;
-    });
-  }
+  Random random = Random();
 
-  void onButton2Clicked() {
-    setState(() {
-      if (widgets.isNotEmpty) {
-        widgets.removeLast();
-        counter -= 1;
-      }
-    });
+  void onAnimatedContainerTap() {
+    setState(() {});
   }
-  // _MyAppState() {
-  //   for (int i = 0; i < 25; i++) {
-  //     widgets.add(Text(
-  //       "Data " + i.toString(),
-  //       style: TextStyle(fontSize: 35),
-  //     ));
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("ListView"),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("ListView"),
+      ),
+      body: Center(
+        child: GestureDetector(
+          child: AnimatedContainer(
+            color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256),
+                random.nextInt(256)),
+            duration: Duration(seconds: 1),
+            width: 50.0 + random.nextInt(101),
+            height: 50.0 + random.nextInt(101),
           ),
-          body: ListView(children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                RaisedButton(
-                    child: Text(
-                      "Increase Text Array",
-                      style: TextStyle(
-                          fontFamily: "Langar",
-                          fontSize: 16,
-                          decoration: TextDecoration.underline),
-                    ),
-                    onPressed: onButton1Clicked),
-                RaisedButton(
-                    child: Text(
-                      "Remove Text Array",
-                      style: TextStyle(
-                        fontFamily: "Langar",
-                        fontSize: 16,
-                        decoration: TextDecoration.underline,
-                        decorationThickness: 5,
-                        decorationColor: Colors.amber,
-                        decorationStyle: TextDecorationStyle.wavy
-                      ),
-                    ),
-                    onPressed: onButton2Clicked),
-              ],
-            ),
-            Column(
-                crossAxisAlignment: CrossAxisAlignment.start, children: widgets)
-          ])),
-    );
+          onTap: onAnimatedContainerTap,
+        ),
+      ),
+    ));
   }
 }
