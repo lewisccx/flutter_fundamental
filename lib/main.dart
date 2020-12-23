25d8ee7ac8407d7e062f16b74cb72968f824b9d7
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-23 11:48:09 +0800

    flexible layout

--- lib/main.dart
+++ lib/main.dart
@@ -22,21 +22,49 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("ListView"),
-      ),
-      body: Center(
-        child: GestureDetector(
-          child: AnimatedContainer(
-            color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256),
-                random.nextInt(256)),
-            duration: Duration(seconds: 1),
-            width: 50.0 + random.nextInt(101),
-            height: 50.0 + random.nextInt(101),
-          ),
-          onTap: onAnimatedContainerTap,
-        ),
-      ),
-    ));
+            appBar: AppBar(
+              title: Text("Flexible Layout"),
+            ),
+            body: Column(children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(5),
+                        color: Colors.amber,
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(5),
+                        color: Colors.yellow,
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(5),
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                flex: 2,
+                child: Container(margin: EdgeInsets.all(5), color: Colors.blue),
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  margin: EdgeInsets.all(5),
+                  color: Colors.deepPurpleAccent,
+                ),
+              )
+            ])));
   }
 }
