3e340c853345ccadd4815607ca91d3c22569fd9c
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-23 12:33:28 +0800

    added stack and align widget

--- lib/main.dart
+++ lib/main.dart
@@ -22,49 +22,200 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: Text("Flexible Layout"),
+      appBar: AppBar(
+        title: Text("Flexible Layout"),
+      ),
+      body: Stack(
+        children: [
+          //background
+          Column(children: [
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(flex: 1, child: Container(color: Colors.amber)),
+                  Flexible(flex: 1, child: Container(color: Colors.green))
+                ],
+              ),
             ),
-            body: Column(children: <Widget>[
-              Flexible(
-                flex: 1,
-                child: Row(
-                  children: [
-                    Flexible(
-                      flex: 1,
-                      child: Container(
-                        margin: EdgeInsets.all(5),
-                        color: Colors.amber,
-                      ),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: Container(
-                        margin: EdgeInsets.all(5),
-                        color: Colors.yellow,
-                      ),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: Container(
-                        margin: EdgeInsets.all(5),
-                        color: Colors.green,
-                      ),
-                    ),
-                  ],
-                ),
+            Flexible(
+              flex: 1,
+              child: Row(
+                children: [
+                  Flexible(flex: 1, child: Container(color: Colors.orange)),
+                  Flexible(flex: 1, child: Container(color: Colors.red))
+                ],
               ),
-              Flexible(
-                flex: 2,
-                child: Container(margin: EdgeInsets.all(5), color: Colors.blue),
-              ),
-              Flexible(
-                flex: 1,
-                child: Container(
-                  margin: EdgeInsets.all(5),
-                  color: Colors.deepPurpleAccent,
+            ),
+          ]),
+          ListView(
+            children: [
+              Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
                 ),
-              )
-            ])));
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Text 1",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ])
+            ],
+          ),
+          Align(
+            //-1 to 0
+            alignment: Alignment(-0.9,0.2),
+            child: RaisedButton(
+                child: Text("click me",style: TextStyle(color: Colors.white),), color: Colors.black, onPressed: () {})),
+          
+        ],
+      ),
+    ));
   }
 }
