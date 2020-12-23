0f1cb4abb158f0b12c2c80aa233c99fefb0376f7
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-23 22:53:14 +0800

    added appbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text("Navigation"),
+        title: Text(
+          "Navigation",
+          style: TextStyle(color: Colors.cyan),
+        ),
+        leading: Icon(Icons.adb, color: Colors.white),
+        actions: [
+          IconButton(icon: Icon(Icons.settings), onPressed: () {}),
+          IconButton(icon: Icon(Icons.exit_to_app), onPressed: () {})
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0xff0096ff), Color(0xff6610f2)],
+                  begin: FractionalOffset.topLeft,
+                  end: FractionalOffset.bottomRight),
+                  image: DecorationImage(image: AssetImage("images/puffer-fish.png"), fit: BoxFit.none, repeat: ImageRepeat.noRepeat)),
+        ),
+      
       ),
       body: LoginPage(),
     ));
