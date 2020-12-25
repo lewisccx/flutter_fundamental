a21e5f5d0a2dff95adebd7db795947a5ee928ed4
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-25 14:17:05 +0800

    added mediaQuery

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fundamental/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,53 +15,44 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("Text Field"),
-      ),
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                fillColor: Colors.lightBlue[50],
-                filled: true,
-                suffixIcon: Icon(Icons.access_alarm),
-                suffixText: "end text",
-                //suffix: ,
-                prefix: Container(
-                  width: 5,
-                  height: 5,
-                  color: Colors.red,
-                ),
-                hintText: "Enter your name",
-                hintStyle: TextStyle(color: Colors.blueGrey, fontWeight: FontWeight.bold) ,
-                icon: Icon(Icons.adb),
-                prefixIcon: Icon(Icons.person),
-                //prefixText: "Name: ",
-                //prefixStyle: TextStyle(color: Colors.blueGrey, fontWeight: FontWeight.bold) ,
-                labelText: "Name",
-                labelStyle: TextStyle(color: Colors.blueGrey, fontWeight: FontWeight.bold) ,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(25)
-                ),
-                
-              ),
-              maxLength: 25,
-              maxLines: 1,
-              obscureText: true,//password
-              onChanged: (value) {
-                setState(() {});
-              },
-              controller: controller,
-            ),
-            Text(controller.text)
-          ],
+    return MaterialApp(home: MainPage());
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("MediaQuery"),
         ),
+        body: (MediaQuery.of(context).orientation == Orientation.portrait)
+            ? Column(children: generateContainers())
+            : Row(children: generateContainers()));
+  }
+
+  List<Widget> generateContainers() {
+    return <Widget>[
+      Container(
+        color: Colors.red,
+        width: 100,
+        height: 100,
       ),
-    ));
+      Container(
+        color: Colors.green,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.blue,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.yellow,
+        width: 100,
+        height: 100,
+      ),
+    ];
   }
 }
