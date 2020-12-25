0629b7b6b8bc8d14f92514e8aa296ce7fa8d0d39
Lewis Cao <33199333+lewisccx@users.noreply.github.com>
2020-12-25 19:23:52 +0800

    added inkwell

--- lib/main.dart
+++ lib/main.dart
@@ -23,36 +23,50 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("MediaQuery"),
+      appBar: AppBar(
+        title: Text("MediaQuery"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            RaisedButton(
+                color: Colors.amber,
+                child: Text("Click me"),
+                shape: StadiumBorder(),
+                onPressed: () {}),
+            Material(
+              borderRadius: BorderRadius.circular(10),
+              elevation: 5,
+              child: Container(
+                width: 150,
+                height: 40,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    gradient: LinearGradient(
+                        colors: [Colors.amber, Colors.orange],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter)),
+                child: Material(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.transparent,
+                  child: InkWell(
+                    splashColor: Colors.red,
+                    borderRadius: BorderRadius.circular(10),
+                    onTap: () {},
+                    child: Center(
+                        child: Text(
+                      "Inkwell button",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w500),
+                    )),
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
-        body: (MediaQuery.of(context).orientation == Orientation.portrait)
-            ? Column(children: generateContainers())
-            : Row(children: generateContainers()));
-  }
-
-  List<Widget> generateContainers() {
-    return <Widget>[
-      Container(
-        color: Colors.red,
-        width: 100,
-        height: 100,
       ),
-      Container(
-        color: Colors.green,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.blue,
-        width: 100,
-        height: 100,
-      ),
-      Container(
-        color: Colors.yellow,
-        width: 100,
-        height: 100,
-      ),
-    ];
+    );
   }
 }
